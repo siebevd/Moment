3d8105ae248815997367fd67125e7c2668b6f414
siebe van dijck <vandijckske@gmail.com>
2017-06-09 01:34:34 +0100

    release of first version

--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 # Moment
+
+Chrome new tab extensions. Don't lose what comes to you in the **moment**.
+
+## Development
+
+1. install `yarn`
+2. run `yarn`
+3. run `yarn dev` to start building
+4. run `yarn prod` to build production version
--- src/components/RandomImage.jsx
+++ src/components/RandomImage.jsx
@@ -9,9 +9,11 @@ class RandomImage extends React.Component {
 	componentWillMount() {
 		getNewPhoto()
 			.then((data)=> {
+				console.log('what is data', data);
 				// Save the image
 				this.setState({
-					image: data.urls.regular
+					image: data.urls.regular,
+					user: data.user
 				})
 
 				// localStorage.setItem('photos', JSON.stringify(data));
@@ -27,6 +29,9 @@ class RandomImage extends React.Component {
 
 		return (
 			<div className="img" style={styles}>
+				{this.state.user && <div className="imgCredit">
+					Photo by <a href={`https://unsplash.com/@${this.state.user.username}?utm_source=moment&utm_medium=referral&utm_campaign=api-credit`}>{this.state.user.name}</a> / <a href={`https://unsplash.com?utm_source=moment&utm_medium=referral&utm_campaign=api-credit`}>Unsplash</a>
+				</div>}
 			</div>
 		)
 	}
